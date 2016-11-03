8762c02f94b5bf3e3d5a596696c1f6192fcf0f8d
Chris Ross <chris.ross@7digital.com>
2016-11-03 11:19:53 +0000

    Initial commit with initial green test

---
+++ DijkstrasAlgorithm.Tests/MinPathTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace DijkstrasAlgorithm.Tests
+{
+    [TestFixture]
+    public class MinPathTest
+    {
+        [Test]
+        public void NoGraph_NoPathZeroLength()
+        {
+            Assert.That(MinPath(""), Is.EqualTo("{}0"));
+        }
+
+        private string MinPath(string graph)
+        {
+            return "{}0";
+        }
+    }
+}
